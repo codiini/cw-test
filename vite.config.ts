9ba4f11fd810a79c6d9965f701296b7f26e7bbcf
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-10-04 13:49:16 +0100

    feat: setup scss variables and mixins

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/styles/_variables.scss";`
+        additionalData: `@import "@/assets/styles/_variables.scss"; @import "@/assets/styles/_mixins.scss";`
       }
     }
   }
